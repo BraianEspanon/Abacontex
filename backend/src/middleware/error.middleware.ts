4a784cbfc13454cc29e45cd9b7e84de23a7db172
Braian Abel Españon Acevedo <braianespanon@gmail.com>
2026-07-10 22:46:54 -0300

    feat: Manejo de errores básico

---
+++ backend/src/middleware/error.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../errors/app-error';
+
+export function errorMiddleware(error: unknown, req: Request, res: Response, next: NextFunction) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      code: error.code,
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: 'error',
+    code: 'INTERNAL_SERVER_ERROR',
+    message: 'Error interno del servidor',
+  });
+}
