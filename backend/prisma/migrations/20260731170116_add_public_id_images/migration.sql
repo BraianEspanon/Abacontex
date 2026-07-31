e5893ee90eefeaa2c9f4502bdfb0fd8e3fc42a7e
NoelAraoz78226 <noelaraoz123@gmail.com>
2026-07-31 14:02:12 -0300

    feat: atributo publicId agregado a empresa, usuario, producto

---
+++ backend/prisma/migrations/20260731170116_add_public_id_images/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "empresas" ADD COLUMN     "logoPublicId" TEXT;
+
+-- AlterTable
+ALTER TABLE "productos" ADD COLUMN     "fotoPublicId" TEXT;
+
+-- AlterTable
+ALTER TABLE "usuarios" ADD COLUMN     "fotoPerfilPublicId" TEXT;
