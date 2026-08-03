4a788aa4280eadaa1af8d9e14135b69a8405121d
Braian <braianespanon@gmail.com>
2026-08-03 01:44:58 -0300

    fix: Quitar console.logs()

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -1,11 +1,6 @@
 import Keycloak from 'keycloak-js';
 import { config } from './config';
 
-// 👇 Agrega esto para cazar el error
-console.log('1. Lo que tiene window:', window.APP_CONFIG);
-console.log('2. Lo que resolvió config.ts:', config);
-console.log('3. Lo que se le va a pasar a Keycloak:', config.KEYCLOAK_URL, config.KEYCLOAK_REALM);
-
 const keycloak = new Keycloak({
   url: config.KEYCLOAK_URL,
   realm: config.KEYCLOAK_REALM,
